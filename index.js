27a13c3719e5c3ac53c7f0ec984a5ffc6b547ebb
laura.broder <laura.broder@fixel.ai>
2022-03-05 19:09:52 +0200

    add /refresh-data endpoint

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require("express");
+const { getOrgRepos, getArtifactsByOrgAndRepo } = require("./axiosClient");
+
+const PORT = 5000;
 const app = express();
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get("/", function (req, res) {
-	res.send("Hello World");
-});
-app.post("/refresh-data", function (req, res) {
-	res.send("refresh-data");
+// router
+app.get("/org/:org_name/daily_runs/:workflow_id", async (req, res) => {
+	const { org_name, workflow_id } = req.params;
+	try {
+	} catch (error) {
+		console.error("file: index.js ~ line 14 ~ app.get ~ error", error.response);
+		res.status(error.response.status).send("Sorry, cant find that. " + error);
+	}
 });
 
-app.listen(5000);
+app.post("/refresh-data", async (req, res) => {
+	const { org_name } = req.body;
+	console.log("file: index.js ~ line 36 ~ app.post ~ org_name", org_name);
+	try {
+		const orgReposRes = await getOrgRepos(org_name);
+		if (!orgReposRes.data.length) {
+			res.status(404).send("no repos were found for this organization");
+		}
+		const promiseArray = orgReposRes.data.map((repo) => {
+			return getArtifactsByOrgAndRepo(org_name, repo.name);
+		});
+		const orgArtifacts = await Promise.all(promiseArray);
+		res.send(orgArtifacts);
+	} catch (error) {
+		res.status(error.response.status).send("there was a problem getting workflow runs for this organization");
+	}
+});
+
+app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
